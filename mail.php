db042b9f8c0bcf799777cba62ad9666238def6fb
MStaffbane <42208427+MStaffbane@users.noreply.github.com>
2024-01-26 13:29:14 +0000

    Changes

--- mail.php
+++ mail.php
@@ -1,25 +1,41 @@
 <?php
 
-	if(isset($_POST['Send']))
-	{
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$subject = $_POST['subject'];
-		$message = $_POST['message'];	
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];	
 		
-		if(empty($name)||empty($Email)||empty($Subject)||empty($Msg))
-		{
-			header('location:index.php?error');
-		}
-		else
-		{
-			$to = "pixel02mark@gmail.com";
-			
-			if(mail($to,$Subject,$Msg,$Email))
-			{
-				header("location:index.php?success");
-			}
-		}
-	}
+$mailheader = "From:".$name."<".$email.">\r\n";
+
+$recipient = "pixel02mark@gmail.com";
+
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
+
+echo'
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact form</title>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Thank you for your message. I will get back to you as soon as possible!</h1>
+        <p class="back">Go back to the <a href="../index.html">homepage</a>.</p>
+        
+    </div>
+</body>
+</html>
+
+
+
+';
+
 
 ?>
