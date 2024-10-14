3c62637817bd17c93925d281ec8b4f81ff085501
MStaffbane <42208427+MStaffbane@users.noreply.github.com>
2024-10-14 16:34:27 +0100

    JS

---
+++ Assets/js/main.js
@@ -0,0 +1,19 @@
+const navMenu = document.getElementById('nav-menu')
+const navOpen = document.getElementById('nav-open')
+const navClose = document.getElementById('nav-close')
+const navItem = document.querySelectorAll('.nav_item')
+navOpen.addEventListener('click', () => {
+    navMenu.classList('nav__menu--open')
+})
+navItem.forEach(item => {
+    item.addEventListener('click', () =>
+        navMenu.classList.remove('nav__menu--open'))
+})
+const header = document.getElementById('l-header')
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50){
+        header.classList.add('l-header--scroll')        
+    } else {
+        header.classList.remove('l-header--scroll')
+    }
+})
