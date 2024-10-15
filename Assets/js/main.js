826db651ef90efdb72826c650c8cbd800427b811
MStaffbane <42208427+MStaffbane@users.noreply.github.com>
2024-10-16 00:59:38 +0100

    changes

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Mobile Menu Toggle
 const navMenu = document.getElementById('nav-menu'),
       navOpen = document.getElementById('nav-open'),
@@ -13,6 +14,30 @@ if(navOpen){
 =======
 document.addEventListener('DOMContentLoaded', function() {
     const homeLink = document.querySelector('a[href="#home"]');
+=======
+const navMenu = document.getElementById('nav-menu')
+const navOpen = document.getElementById('nav-open')
+const navClose = document.getElementById('nav-close')
+const navItem = document.querySelectorAll('.nav_item')
+navOpen.addEventListener('click', () => {
+    navMenu.classList('nav__menu--open')
+})
+navItem.forEach(item => {
+    item.addEventListener('click', () =>
+        navMenu.classList.remove('nav__menu--open'))
+})
+const header = document.getElementById('l-header')
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50){
+        header.classList.add('l-header--scroll')        
+    } else {
+        header.classList.remove('l-header--scroll')
+    }
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('.nav__link');
+>>>>>>> parent of a461a7a (nav)
     
     if (homeLink) {
         homeLink.addEventListener('click', function(e) {
